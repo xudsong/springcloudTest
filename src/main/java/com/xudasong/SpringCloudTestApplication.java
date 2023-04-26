1d0c3f8377418560e5be0697292a57c63e89a13f
song <xudsong@163.com>
2023-04-26 22:28:43 +0800

    默认

---
+++ src/main/java/com/xudasong/SpringCloudTestApplication.java
@@ -0,0 +1,13 @@
+package com.xudasong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTestApplication.class, args);
+	}
+
+}
